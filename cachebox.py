06cc87b913ed17e47c400e82aec5bd2e5f64409f
Stefan <st3f4n.s@gmail.com>
2026-08-12 11:21:17 +0200

    Add cachebox

---
+++ cachebox.py
@@ -0,0 +1,19 @@
+import threading
+
+_lock = threading.Lock()
+_box = {}
+
+
+def store(key, value):
+    with _lock:
+        _box[key] = value
+
+
+def fetch(key, loader):
+    """Return the cached value, loading it on a miss."""
+    _lock.acquire()
+    if key in _box:
+        value = _box[key]
+        _lock.release()
+        return value
+    return loader(key)
