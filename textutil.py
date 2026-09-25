968f6697afc58da4c1cd1280c29f7611277ac312
Stefan <stefan@starflinger.eu>
2026-09-25 15:28:57 +0200

    Add lines helper

--- textutil.py
+++ textutil.py
@@ -1,3 +1,8 @@
 def words(text):
     """Split text on whitespace into a list of words."""
     return text.split()
+
+
+def lines(text):
+    """Split text into lines without trailing newlines."""
+    return text.splitlines()
